8728ab3297269ea26e6a28f0133e543bb119f69f
Nathan Rempel <nsrempel@gmail.com>
2018-08-22 18:14:32 +1000

    Make menu scatter work. restructure components and containers

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,3 +1,7 @@
 export * from './card/collapsable';
 export * from './referendum/referendum';
+export * from './layout/header/header';
+export * from './layout/menu/menu';
+export { default as Error503 } from './error/503.rt';
+export { default as Footer } from './layout/footer.rt';
 export { Fragment } from 'react';
--- src/app/containers/index.js
+++ src/app/containers/index.js
@@ -1,5 +1,1 @@
 export { default as Home } from './home/home';
-export * from './layout/header/header';
-export * from './layout/menu/menu';
-export { default as Error503 } from './error/503.rt';
-export { default as Footer } from './layout/footer.rt';
