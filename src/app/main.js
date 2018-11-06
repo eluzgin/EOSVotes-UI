f8343e140b237cf76ffcd8481b212f0e7e6f3cc7
Denis Carriere <carriere.denis@gmail.com>
2018-11-06 17:31:21 -0500

    Add Foo Bar router

--- src/app/main.js
+++ src/app/main.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import configureStore from 'store/configureStore'
 import App from './app';
+import Foo from './containers/foo/foo';
 
 const store = configureStore();
 
 function render(){
   ReactDOM.render(
     <Provider store={store}>
-      <App/>
+      <Router>
+        <div>
+          <Route exact path="/" component={App} />
+          <Route path="/foo" component={Foo} />
+        </div>
+      </Router>
     </Provider>,
     window.document.getElementById('root'));
 }
